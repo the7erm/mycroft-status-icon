7cff675d07155699eab20237a6ddab6df3ef2450
Eugene R. Miller <theerm@gmail.com>
2016-06-27 06:25:36 -0600

    added liscence and requirements

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ icon changes to recording when mycroft is listening, a gear when he's
 processing, and everything he says gets displayed in a notification.  This
 is useful for debugging.
 
-The host/port `ws://127.0.0.1:9000/events/ws`is hardcoded.  You'll need to 
-change it.  Perhaps in the future I'll have look in `/etc/` and 
+The host/port `ws://127.0.0.1:9000/events/ws`is hardcoded.  You'll need to
+change it.  Perhaps in the future I'll have look in `/etc/` and
 `~/.mycroft/mycroft.ini`.
 
 # Known bugs
 - Doesn't reconnect quite right when you restart the service.
 
+# Requirements
+```
+sudo apt-get install python-gtk2 libnotify-bin pip
+```
+
+## python requirements
+`pip install -r requirements.txt`
--- mycroft_icon.py
+++ mycroft_icon.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+# Copyright 2016 Eugene R. Miller
+#
+# Mycroft icon is free software: you can redistribute
+# it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Icon is distributed in the hope that it will
+#  be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the Mycroft Icon.
+# If not, see <http://www.gnu.org/licenses/>.
+
+
 import gi
 from gi.repository import Gtk
 from pprint import pprint
